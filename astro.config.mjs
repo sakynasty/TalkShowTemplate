1111ba9ca4c0f72762e5232a60adf5db7fddef39
Spyzilya <135563763+XanaCode@users.noreply.github.com>
2023-09-02 15:48:44 +0200

    Fix problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vsp-demo.pages.dev"
-  /*server: {
-    port: 3000
-  },*/
+  site: "https://vsp-demo.pages.dev",
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare({ mode: 'directory' })
+  adapter: cloudflare({ mode: 'directory', functionPerRoute: true })
 });
