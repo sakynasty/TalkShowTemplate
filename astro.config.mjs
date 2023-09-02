5f1ad8b6ad122e459abe7cccc1fe57aec01bed2d
Spyzilya <135563763+XanaCode@users.noreply.github.com>
2023-09-02 15:41:55 +0200

    Prepare for Demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
+  host: "vsp-demo.pages.dev"
+  /*server: {
     port: 3000
-  },
-  integrations: [tailwind()]
+  },*/
+  integrations: [tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
