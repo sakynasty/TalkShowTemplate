ae0a7a3af69bfffe75040d4facd3b1c758ce00e3
byFrench <135563763+byFrench@users.noreply.github.com>
2023-09-29 22:21:09 +0200

    👀 Remove old adapter & site host.
    Old adapter & host for the old demonstration via CloudFlare Page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vsp-demo.pages.dev",
   server: {
     port: 3000
   },
   integrations: [tailwind()],
-  output: "server",
-  adapter: cloudflare({ mode: 'directory' })
+  output: "server"
 });
