658d7050a65e308bfcfa3467626ff5962ff71aa4
Sakynasty <135563763+sakynasty@users.noreply.github.com>
2024-01-29 22:42:54 +0100

    Prepare for demo site with cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+	site: 'talkshowtemplate.pages.dev',
 	server: {
 		port: 3000
 	},
 	integrations: [tailwind()],
-	output: 'server'
+	output: 'server',
+	adapter: cloudflare({
+		mode: 'directory'
+	}),
+	compressHTML: true
 });
