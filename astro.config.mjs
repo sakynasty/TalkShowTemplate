d9bf3bd286b4eb20373c5175122aedb7d954f167
Sakynasty <135563763+sakynasty@users.noreply.github.com>
2024-01-29 21:24:44 +0100

    Format last update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  integrations: [tailwind()],
-  output: "server",
+	server: {
+		port: 3000
+	},
+	integrations: [tailwind()],
+	output: 'server'
 });
